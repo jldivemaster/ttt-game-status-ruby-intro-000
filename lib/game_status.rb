325d192a8b8e52fd30b4dcebe8fa21fb1a5e0e1f
Jason Leach <jlspursfan@gmail.com>
2019-04-24 16:04:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,7 +42,7 @@ end
 
 
 def draw?(board)
-  if ( board.all?{ |i| (i == "X" || i == "O" ) } ) && !(won?(board))
+  if ( board.all?{ |i| (i == "X" || i == "O" ) } )
     return true
   else
     return false
