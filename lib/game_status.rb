b8ec966f52085b64894328d72b5fd34babe8e875
Jason Leach <jlspursfan@gmail.com>
2019-04-24 17:44:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -66,18 +66,19 @@ def winner(board)
   if won?(board)
     win = WIN_COMBINATIONS.each do |win_combination|
 
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
 
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
+      position_1 = board[win_index_1] # load the value of the board at win_index_1
+      position_2 = board[win_index_2] # load the value of the board at win_index_2
+      position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X")
-      return "X"
-    elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return "O"
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        return "X"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return "O"
+      end
     end
   end
 end
