388120702546396a78e4ed87e1664e5cbbd02a4e
Jason Leach <jlspursfan@gmail.com>
2019-04-24 15:44:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,52 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+def won?(board)
+  trw = WIN_COMBINATIONS[0]
+
+  if board.all?{ |i| i == " " || i == "" || i == nil }
+    return false
+  end
+
+  if ((board(trw[0]) == "X") && (board(trw[1]) == "X") && (board(trw[2]) == "X")) || ((board(trw[0]) == "O") && (board(trw[1]) == "O") && (board(trw[2]) == "O"))
+    return trw
+  else
+    return false
+  end
+
+end
+
+def full?(board)
+  if ( board.all?{ |i| (i == "X" || i == "O" ) } )
+    return true
+  else
+    return false
+  end
+end
+
+
+def draw?(board)
+
+end
+
+
+def over?(board)
+  if (won?(board) == true) || (board.all?{ |i| (i == "X" || i == "O") } )
+    return true
+  else
+    return false
+  end
+end
+
+
+def winner(board)
+  if won?(board) && position_1 == "X"
+    return "X"
+  end
+
+  if won?(board) && position_1 == "O"
+    return "O"
+  end
+end
