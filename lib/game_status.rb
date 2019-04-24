98640564cec22dddd8bb1f53962d4718e86abc46
Jason Leach <jlspursfan@gmail.com>
2019-04-24 17:04:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -78,7 +78,7 @@ def winner(board)
     elsif won?(board) && (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return "O"
     else
-      return "nil"
+      "nil"
     end
   end
 end
