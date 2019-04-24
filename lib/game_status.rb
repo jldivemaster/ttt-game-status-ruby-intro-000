8843f1ea9df9e72398a699083ed5c9b12ac6b0f1
Jason Leach <jlspursfan@gmail.com>
2019-04-24 16:53:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,9 +21,9 @@ def won?(board)
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      return win_combination + "X"
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      return win_combination + "O"
     else
       false
     end
