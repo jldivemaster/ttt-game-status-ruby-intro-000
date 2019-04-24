5cfb095c51e0e55c60232fd6e42e41c3d5e52ab5
Jason Leach <jlspursfan@gmail.com>
2019-04-24 16:00:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,22 +7,23 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 def won?(board)
-  #trw = WIN_COMBINATIONS[0]
+  trw = WIN_COMBINATIONS[0]
 
   if board.all?{ |i| i == " " || i == "" || i == nil }
     return false
   end
 
-  WIN_COMBINATIONS.each do |sub_arr|
-    sub_arr.each do |idx|
-      position = board[idx]
-      if position.all?{ |i| (i == "X") || (i == "O") }
-        return sub_arr
-      else
-        return false
-      end
+#  WIN_COMBINATIONS.each do |sub_arr|
+  trw.each do |idx|
+    position = board[idx]
+
+    if position.all?{ |i| (i == "X") || (i == "O") }
+      return trw
+    else
+      return false
     end
   end
+  #end
       #if(board(trw[1]) == "X") && (board(trw[2]) == "X")) ((board[idx] == "X") && (board[trw[1]] == "X") && (board[trw[2]] == "X")) || ((board[trw[0]] == "O") && (board[trw[1]] == "O") && (board[trw[2]] == "O"))
   #  return trw
   #else
